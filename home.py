3cc8800e8889691dcc9887952a30e6836b95e300
Andrew Siah <andrew.siah@abra.com>
2023-03-04 16:20:28 -0700

    added goplus.py

--- home.py
+++ home.py
@@ -10,5 +10,11 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://swap.cow.fi/#/1/swap/WETH", height=600)
+components.iframe(
+    "https://pancakeswap.finance/add/BNB/0x55d398326f99059ff775485246999027b3197955",
+     height=600)
 
+# goplusapi
+contract_security = requests.get("")
+
+0x16b9a82891338f9bA80E2D6970FddA79D1eb0daE
---
+++ modules/goplus.py
@@ -0,0 +1,5 @@
+import web3
+import requests
+import pandas as pd
+import asyncio
+import json
