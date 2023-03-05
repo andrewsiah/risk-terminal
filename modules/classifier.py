47adb2808772f203b4bb8184c2d5d8df61d259cd
manas <bagrimanasbir@gmail.com>
2023-03-05 05:35:22 -0700

    a

--- modules/classifier.py
+++ modules/classifier.py
@@ -14,6 +14,7 @@ net_worth = pd.read_json("output/balances.txt")
 is_lp = pd.read_json("output/addresses.txt")
 is_trader = pd.read_json("output/addresses2.txt")
 net_worth2 = pd.read_json("output/balances2.txt")
+avg_trade_size = pd.read_json("output/avg_trade_size.txt")
 
 classifications = { "labels": {} }
 for i in trade_sizes[0]:
@@ -27,6 +28,8 @@ for i in is_lp[0]:
 for i in is_trader[0]:
     classifications["labels"][i] = []
 for i in net_worth2[0]:
+    classifications["labels"][i] = []
+for i in avg_trade_size[0]:
     classifications["labels"][i] = []
 
 
@@ -56,8 +59,20 @@ for i in range(len(net_worth2[0])):
 for i in range(len(trade_sizes[0])):
     if int(trade_sizes[1][i]) > 50: #if large and frequent trader, then most likely an arb
         classifications["labels"][trade_sizes[0][i]].append("Abritrageur")
-    elif int(trade_sizes[1][i]) < 10 and classifications["labels"][net_worth2[0][i]][0] != "Whale":
-        #classifications["labels"][trade_sizes[0][i]].append("Retail")
+    elif int(trade_sizes[1][i]) > 10:
+        classifications["labels"][trade_sizes[0][i]].append("Day Trader")
+for i in range(len(num_of_trades[0])):
+    if int(num_of_trades[1][i]) > 3:
+        classifications["labels"][num_of_trades[0][i]].append("Day Trader")
+    elif int(num_of_trades[1][i]) > 1:
+        classifications["labels"][num_of_trades[0][i]].append("HODLer")
+for i in range(len(num_of_trades[0])):
+    if int(num_of_trades[1][i]) > 40:
+        classifications["labels"][num_of_trades[0][i]].append("Abritrageur")
+    if int(num_of_trades[1][i]) > 10:
+        classifications["labels"][num_of_trades[0][i]].append("Day Trader")
+    elif int(num_of_trades[1][i]) > 1:
+        classifications["labels"][num_of_trades[0][i]].append("HODLer")
 
 
 # print(classifications["labels"])
